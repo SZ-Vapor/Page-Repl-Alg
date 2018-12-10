7fc99b6e9950a80cd24cea6dd7423deff0112b79
Steven Hogenson <33229415+SZ-Vapor@users.noreply.github.com>
2018-12-09 21:28:28 -0500

    Update LRU.java

--- LRU.java
+++ LRU.java
@@ -15,37 +15,34 @@ public class LRU extends ReplacementAlgorithm {
 
     public ArrayList<Integer> list;	//the list to keep track of the frames
     private int maxFrameCount;			//the maximum size of the list
-   
+
     public LRU(int pageFrameCount) {
         super(pageFrameCount);
         maxFrameCount = pageFrameCount;
-        //list = new ArrayList<>(frameCountMax);
         list = new ArrayList<>();
+    }
+
+    public void resetData(){
+        list.clear();
+        pageFaultCount = 0;
     }
 
     @Override
     public void insert(int pageNumber) {
- if (list.contains(pageNumber)) {
-            //bubble this page to the bottom
-            //list.remove(list.size()-1);
+        if (list.contains(pageNumber)) {
             list.remove(list.indexOf(pageNumber));
-            //list.add(0, pageNumber);//old
-            list.add(pageNumber);//new
+            list.add(pageNumber);
             return;
         }
 
         //if the stack is full, pop the old page and push the new page
         if (list.size() >= maxFrameCount) {
-            //list.remove(list.size() - 1);//old
-            list.remove(0);//new
-            //stack.addLast(pageNumber);
-            //list.add(0, pageNumber);//old
-            list.add(pageNumber);//new
+            list.remove(0);
+            list.add(pageNumber);
             pageFaultCount++;
         } //if the stack isn't full, just push the page
         else {
-            //list.add(0, pageNumber);//old
-            list.add(pageNumber);//new
+            list.add(pageNumber);
             pageFaultCount++;
         }
 
